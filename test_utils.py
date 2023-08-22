85d72045ceb4acfab82091cf895b7f41c9806984
hugo espinosa <hugoa.espinosag@gmail.com>
2023-08-22 17:48:37 +0200

    Update Test_Utils

--- test_utils.py
+++ test_utils.py
@@ -1,10 +1,10 @@
 from utils import es_primo
 
 def test_es_primo():
-    assert es_primo(2) == True
-    assert es_primo(3) == True
-    assert es_primo(17) == True
-    assert es_primo(4) == False
-    assert es_primo(9) == False
-    assert es_primo(29) == True
-    assert es_primo(100) == False
+    assert es_primo(2)
+    assert es_primo(3)
+    assert es_primo(17)
+    assert not es_primo(4)
+    assert not es_primo(9)
+    assert es_primo(29)
+    assert not es_primo(100)
